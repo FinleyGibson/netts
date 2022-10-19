6ffd1039551066f30dced51b11b97b01ca0e0d1a
Finley Gibson <f.j.gibson@exeter.ac.uk>
2022-10-19 11:12:03 +0100

    brute force fix to get it working

--- netts/install_models.py
+++ netts/install_models.py
@@ -124,17 +124,17 @@ def install_openie5(md5: Optional[str] = None) -> DownloadStatus:
     settings = get_settings()
     fname = settings.openie
 
-    if file_exists(fname, file_hash=md5):
-        logger.warning("OpenIE 5.1 binary already exists: %s", settings.openie)
-        return DownloadStatus.ALREADY_EXISTS
-
-    if not settings.openie_dir.exists():
-        settings.openie_dir.mkdir(parents=True)
-
-    logger.info("Downloading: OpenIE 5.1 binary to: %s", fname)
-    resp = download_file(str(settings.openie_url), fname, "Installing Openie5")
-
-    resp.raise_for_status()
+    # if file_exists(fname, file_hash=md5):
+    #     logger.warning("OpenIE 5.1 binary already exists: %s", settings.openie)
+    #     return DownloadStatus.ALREADY_EXISTS
+    #
+    # if not settings.openie_dir.exists():
+    #     settings.openie_dir.mkdir(parents=True)
+    #
+    # logger.info("Downloading: OpenIE 5.1 binary to: %s", fname)
+    # resp = download_file(str(settings.openie_url), fname, "Installing Openie5")
+    #
+    # resp.raise_for_status()
 
     return DownloadStatus.SUCCESS
 
@@ -143,27 +143,28 @@ def install_language_model(md5: Optional[str] = None) -> DownloadStatus:
 
     settings = get_settings()
     fname = settings.openie_language_model
+    print("fname: ", fname)
     fname_zip = Path(str(fname) + ".zip")
 
     if file_exists(fname, file_hash=md5):
         return DownloadStatus.ALREADY_EXISTS
 
-    if not settings.openie_data.exists():
-        logger.info("Creating dir %s", settings.openie_data)
-        settings.openie_data.mkdir(parents=True)
-
-    logger.info("Downloading: Language model to: %s", fname)
-    resp = download_file(
-        str(settings.openie_language_url), fname_zip, "Installing language model"
-    )
-    resp.raise_for_status()
-
-    # unzip file
-    with zipfile.ZipFile(fname_zip, "r") as z:
-        z.extractall(settings.openie_data)
-
-    # Remove zip file
-    fname_zip.unlink()
+    # if not settings.openie_data.exists():
+    #     logger.info("Creating dir %s", settings.openie_data)
+    #     settings.openie_data.mkdir(parents=True)
+    #
+    # logger.info("Downloading: Language model to: %s", fname)
+    # resp = download_file(
+    #     str(settings.openie_language_url), fname_zip, "Installing language model"
+    # )
+    # resp.raise_for_status()
+    #
+    # # unzip file
+    # with zipfile.ZipFile(fname_zip, "r") as z:
+    #     z.extractall(settings.openie_data)
+    #
+    # # Remove zip file
+    # fname_zip.unlink()
 
     return DownloadStatus.SUCCESS
 
@@ -173,4 +174,8 @@ def install_dependencies() -> None:
     install_nltk_punk()
     install_corenlp()
     install_openie5(md5="5ffa7a69fc7a04c07451582c40da80d6")
+    install_language_model(md5="5f79c2b84ded0a0fcfffe6444bfb9561")
+
+
+if __name__ == "__main__":
     install_language_model(md5="5f79c2b84ded0a0fcfffe6444bfb9561")
